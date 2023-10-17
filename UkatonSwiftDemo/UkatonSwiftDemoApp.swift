318424017a22a4c06dca06f84c134564344dc9a6
Zack <zack@ukaton.com>
2023-10-16 22:23:39 -0700

    Initial Commit

---
+++ UkatonSwiftDemo/UkatonSwiftDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  UkatonSwiftDemoApp.swift
+//  UkatonSwiftDemo
+//
+//  Created by Zack Qattan on 10/16/23.
+//
+
+import SwiftUI
+
+@main
+struct UkatonSwiftDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
