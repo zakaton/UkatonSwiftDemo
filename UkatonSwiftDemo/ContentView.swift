91f68bcdd5556d3e2ccbd106ab4d21450ef4ea36
Zack <zack@ukaton.com>
2023-10-30 21:50:21 -0700

    /

--- UkatonSwiftDemo/ContentView.swift
+++ UkatonSwiftDemo/ContentView.swift
@@ -9,7 +9,6 @@ import SwiftUI
 import UkatonKit
 
 struct ContentView: View {
-    let ukatonMission = BLEUkatonMission()
     var body: some View {
         VStack {
             Image(systemName: "globe")
