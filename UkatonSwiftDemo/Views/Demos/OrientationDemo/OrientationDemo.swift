cd4139c70a8b944d5222fe2164ae4960dc168857
Zack <zack@ukaton.com>
2023-11-09 12:23:02 -0800

    .

--- UkatonSwiftDemo/Views/Demos/OrientationDemo/OrientationDemo.swift
+++ UkatonSwiftDemo/Views/Demos/OrientationDemo/OrientationDemo.swift
@@ -1,3 +1,6 @@
+import Combine
+import ModelIO
+import SceneKit
 import SwiftUI
 import UkatonKit
 
@@ -8,9 +11,53 @@ import UkatonKit
 
 struct OrientationDemo: View {
     @ObservedObject var mission: UKMission
+    @State private var newSensorDataConfigurations: UKSensorDataConfigurations = .init()
+
+    var scene: SCNScene = .init()
+    var model: SCNScene
+    var lightNode: SCNNode = .init()
+
+    var cameraNode: SCNNode? {
+        scene.rootNode.childNode(withName: "camera", recursively: false)
+    }
+
+    var isQuaternionEnabled: Bool {
+        mission.sensorDataConfigurations.motion[.quaternion]! > 0
+    }
+
+    init(mission: UKMission) {
+        self.mission = mission
+
+        model = .init(named: "leftShoe.usdz")!
+        scene.rootNode.addChildNode(model.rootNode)
+
+        lightNode.light = .init()
+        lightNode.light!.type = .ambient
+        scene.rootNode.addChildNode(lightNode)
+    }
 
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            SceneView(scene: scene)
+            HStack {
+                Spacer()
+                VStack {
+                    Button(action: {
+                        newSensorDataConfigurations.motion[.quaternion] = isQuaternionEnabled ? 0 : 20
+
+                        try? mission.setSensorDataConfigurations(newSensorDataConfigurations)
+                    }, label: {
+                        if isQuaternionEnabled {
+                            Text("disable")
+                        }
+                        else {
+                            Text("enable")
+                        }
+                    })
+                    Spacer()
+                }
+            }
+        }
     }
 }
 
