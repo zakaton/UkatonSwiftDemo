0a729c3049441600b4fae0871e890fd2bb8508a1
Zack <zack@ukaton.com>
2023-11-21 12:35:36 -0800

    basic scan button

--- (Mac) Ukaton Demo Safari Extension/Resources/background.js
+++ (Mac) Ukaton Demo Safari Extension/Resources/background.js
@@ -1,5 +1,23 @@
+var isScanning = false;
+function toggleScan() {
+    isScanning = !isScanning
+}
+
 browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
     console.log("Received request: ", request);
+    
+    switch (request.type) {
+        case "isScanning":
+            sendResponse({ isScanning });
+            break;
+        case "toggleScan":
+            toggleScan();
+            sendResponse({ isScanning });
+            break;
+        default:
+            console.log("uncaught request type", request.type);
+            break;
+    }
 
     if (request.greeting === "hello")
         sendResponse({ farewell: "goodbye" });
--- (Mac) Ukaton Demo Safari Extension/Resources/popup.js
+++ (Mac) Ukaton Demo Safari Extension/Resources/popup.js
@@ -1,1 +1,19 @@
-console.log("Hello World!", browser);
+var isScanning = false;
+function setIsScanning(newIsScanning) {
+    console.log("set isScannng", newIsScanning)
+    isScanning = newIsScanning
+    toggleScanButton.innerText = isScanning? "stop scan":"scan"
+}
+
+const toggleScanButton = document.getElementById("toggleScan")
+toggleScanButton.addEventListener("click", () => {
+    browser.runtime.sendMessage({ type: "toggleScan" }).then((response) => {
+        console.log("toggleScan response: ", response);
+        setIsScanning(response.isScanning)
+    });
+})
+
+browser.runtime.sendMessage({ type: "isScanning" }).then((response) => {
+    console.log("isScanning response: ", response);
+    setIsScanning(response.isScanning)
+});
